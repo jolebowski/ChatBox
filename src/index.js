30d3e5f31b4e71e7b6add07b2e28992e38f5d02e
jolebowski <jobogos@gmail.com>
2018-07-15 02:18:34 +0200

    config of reactjs + add components

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//REACT 
+
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//Components 
+
+import Connexion from './components/Connexion';
+
+import App from './components/App';
+
+//CSS
+
+import './index.css'
+
+render(
+    <App />,
+    document.getElementById('root')
+)
+
