461458a30b8d7b553e2fbdf4d06e022a5754926a
jolebowski <jobogos@gmail.com>
2018-07-19 01:11:18 +0200

    mise en place de react-router

--- src/index.js
+++ src/index.js
@@ -10,12 +10,30 @@ import Connexion from './components/Connexion';
 
 import App from './components/App';
 
+import NotFound from './components/NotFound'
+
 //CSS
 
 import './index.css'
 
+//router
+
+import { BrowserRouter, Match, Miss }from 'react-router'; 
+
+const Root = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={Connexion}/>
+                <Match pattern="/pseudo/:pseudo" component={App}/>
+                <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>
+    )
+}
+
 render(
-    <App />,
+    <Root />,
     document.getElementById('root')
 )
 
